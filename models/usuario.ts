12fd2c7872a88d0e4437fc8771309e17dca2fb51
deividR311 <deicen24@gmail.com>
2020-05-07 22:20:10 -0500

    first

--- models/usuario.ts
+++ models/usuario.ts
@@ -3,7 +3,11 @@ import { Schema } from 'mongoose';
 
 const usuarioSchema = new Schema({
 
-    nombre: {
+    name: {
+        type: String,
+        required: [true, 'el nombre es necesario']
+    },
+    lastName: {
         type: String,
         required: [true, 'el nombre es necesario']
     }
